e9361818b39b5f0ea5443ac55e22118a3bb73741
dariak <dariak@playtika.com>
2020-10-14 16:09:12 +0300

    Split screen, split axises

--- CustomUAProject/Assets/FPSMovement.cs
+++ CustomUAProject/Assets/FPSMovement.cs
@@ -10,56 +10,62 @@ public class FPSMovement : MonoBehaviour
     private Quaternion _cameraRot;
     private Transform _cameraTransform;
     private readonly float rotationClamp = 40;
-    private Camera _mainCamera;
+    [SerializeField] private Camera _mainCamera;
     private float _defaultZoom;
     private float _cutZoom;
     private readonly float zoomSpeed = 4f;
     [SerializeField] private Camera gunCamera;
     [SerializeField] private float moveSpeed = 15f;
     [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private ControllAxis controllAxis;
+    private string verticalAxis, horizontalAxis;
+    public bool mouseActive = true;
 
     private void Awake()
     {
         _transform = transform;
+        SetAxis(controllAxis);
     }
 
     private void Start()
     {
-        if (!(Camera.main is null))
-        {
-            _mainCamera = Camera.main;
-            _cameraTransform = _mainCamera.transform;
-            _cameraRot = _cameraTransform.rotation;
-            _defaultZoom = _mainCamera.fieldOfView;
-            _cutZoom = _defaultZoom / 2;
-        }
+        _cameraTransform = _mainCamera.transform;
+        _cameraRot = _cameraTransform.rotation;
+        _defaultZoom = _mainCamera.fieldOfView;
+        _cutZoom = _defaultZoom / 2;
+        
     }
 
     private void Update()
     {
-        var forwardMove = _transform.forward * (Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
-        var horizontalMove = _transform.right * (Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
-        var horizontalRotation = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
-        _transform.position = _transform.position + forwardMove + horizontalMove;
-        _transform.rotation *= Quaternion.Euler(new Vector3(0, horizontalRotation, 0));
-
-        var verticalRotation = - Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
-        _cameraRot *= Quaternion.Euler(new Vector3(verticalRotation, 0, 0));
-
-        _cameraRot = LockCameraMovement(_cameraRot);
-        _cameraTransform.localRotation = _cameraRot;
-
-        float currentZoom = _mainCamera.fieldOfView;
+        var forwardMove = _transform.forward * (Input.GetAxis(verticalAxis) * moveSpeed * Time.deltaTime);
+        var horizontalMove = _transform.right * (Input.GetAxis(horizontalAxis) * moveSpeed * Time.deltaTime);
         
-        if (Input.GetMouseButton(1))
+        _transform.position = _transform.position + forwardMove + horizontalMove;
+
+        if (mouseActive)
         {
-            _mainCamera.fieldOfView = Mathf.Lerp(currentZoom, _cutZoom, zoomSpeed * Time.deltaTime);
-            gunCamera.fieldOfView = Mathf.Lerp(currentZoom, _cutZoom, zoomSpeed * Time.deltaTime);
-        }
-        else
-        {
-            _mainCamera.fieldOfView = Mathf.Lerp(currentZoom, _defaultZoom, zoomSpeed * Time.deltaTime);
-            gunCamera.fieldOfView = Mathf.Lerp(currentZoom, _defaultZoom, zoomSpeed * Time.deltaTime);
+            var horizontalRotation = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+            _transform.rotation *= Quaternion.Euler(new Vector3(0, horizontalRotation, 0));
+
+            var verticalRotation = -Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+            _cameraRot *= Quaternion.Euler(new Vector3(verticalRotation, 0, 0));
+
+            _cameraRot = LockCameraMovement(_cameraRot);
+            _cameraTransform.localRotation = _cameraRot;
+
+            float currentZoom = _mainCamera.fieldOfView;
+
+            if (Input.GetMouseButton(1))
+            {
+                _mainCamera.fieldOfView = Mathf.Lerp(currentZoom, _cutZoom, zoomSpeed * Time.deltaTime);
+                gunCamera.fieldOfView = Mathf.Lerp(currentZoom, _cutZoom, zoomSpeed * Time.deltaTime);
+            }
+            else
+            {
+                _mainCamera.fieldOfView = Mathf.Lerp(currentZoom, _defaultZoom, zoomSpeed * Time.deltaTime);
+                gunCamera.fieldOfView = Mathf.Lerp(currentZoom, _defaultZoom, zoomSpeed * Time.deltaTime);
+            }
         }
     }
     
@@ -76,4 +82,27 @@ public class FPSMovement : MonoBehaviour
  
         return q;
     }
+
+    private void SetAxis(ControllAxis _controllAxis)
+    {
+        switch (_controllAxis)
+        {
+            case ControllAxis.Arrows:
+                verticalAxis = "ArrowsUD";
+                horizontalAxis = "ArrowsLR";
+                break;
+            case ControllAxis.WASD:
+                verticalAxis = "WS";
+                horizontalAxis = "AD";
+                break;
+        }
+        
+
+    }
+}
+
+public enum ControllAxis
+{
+    Arrows,
+    WASD
 }
